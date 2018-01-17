553feca781090a77765372b3f4305bec2f03475f
Zach Mullen <zach.mullen@kitware.com>
2018-01-17 13:04:56 -0500

    Add a few medical VR presets to the script

--- preprocess/process_volume.py
+++ preprocess/process_volume.py
@@ -18,10 +18,81 @@ DEFAULT_HEIGHT = 512
 # the global namespace, presumably because they are weakly linked by VTK
 ctypes.CDLL('libOSMesa.so', ctypes.RTLD_GLOBAL)
 
+# Copied from Slicer:
+# https://github.com/Slicer/Slicer/blob/master/Modules/Loadable/VolumeRendering/Resources/presets.xml
+MEDICAL_XFER_PRESETS = {
+    'CT-AAA': {
+        'rgba': (
+            (-3024, 0, 0, 0, 0),
+            (143.556, 0.615686, 0.356863, 0.184314, 0),
+            (166.222, 0.882353, 0.603922, 0.290196, 0.686275),
+            (214.389, 1, 1, 1, 0.696078),
+            (419.736, 1, 0.937033, 0.954531, 0.833333),
+            (3071, 0.827451, 0.658824, 1, 0.803922)
+        ),
+        'shade': True,
+        'ambient': 0.1,
+        'diffuse': 0.9,
+        'specular': 0.2,
+        'specularPower': 10
+    },
+    'CT-Bones': {
+        'rgb': (
+            (-1000, 0.3, 0.3, 1),
+            (-488, 0.3, 1, 0.3),
+            (463.28, 1, 0, 0),
+            (659.15, 1, 0.912535, 0.0374849),
+            (953, 1, 0.3, 0.3)
+        ),
+        'opacity': (
+            (-1000, 0),
+            (152.19, 0),
+            (278.93, 0.190476),
+            (952, 0.2)
+        ),
+        'shade': True,
+        'ambient': 0.2,
+        'diffuse': 1,
+        'specular': 0,
+        'specularPower': 1
+    },
+    'CT-Soft-Tissue': {
+        'rgba': (
+            (-2048, 0, 0, 0, 0),
+            (-167.01, 0, 0, 0, 0),
+            (-160, 0.0556356, 0.0556356, 0.0556356, 1),
+            (240, 1, 1, 1, 1),
+            (3661, 1, 1, 1, 1)
+        ),
+        'shade': False,
+        'ambient': 0.2,
+        'diffuse': 1,
+        'specular': 0,
+        'specularPower': 1
+    }
+}
 
 def arc_samples(n_samples, sample_range=360., start=0.):
     step = sample_range / n_samples
     return [start + step * i for i in range(n_samples)]
+
+
+def build_xfer_fns(color_fn, opacity_fn, volume_property, data):
+    if 'rgba' in data:
+        for pt in data['rgba']:
+            color_fn.AddRGBPoint(*pt[:4])
+            opacity_fn.AddPoint(pt[0], pt[4])
+    else:
+        for pt in data['rgb']:
+            color_fn.AddRGBPoint(*pt)
+        for pt in data['opacity']:
+            opacity_fn.AddPoint(*pt)
+
+    volume_property.SetAmbient(data['ambient'])
+    volume_property.SetDiffuse(data['diffuse'])
+    volume_property.SetSpecular(data['specular'])
+    if data['shade']:
+        volume_property.ShadeOn()
 
 
 @click.command()
@@ -31,8 +102,9 @@ def arc_samples(n_samples, sample_range=360., start=0.):
 @click.option('--height', default=DEFAULT_HEIGHT, help='output image height (px)')
 @click.option('--phi-samples', default=12, help='number of samples in phi dimension')
 @click.option('--theta-samples', default=3, help='number of samples in theta dimension')
+@click.option('--xfer-preset', default=None, help='transfer function preset to use')
 @click.version_option(version=__version__, prog_name='Process a volume image into a 3d thumbnail')
-def process(in_file, out_dir, width, height, phi_samples, theta_samples):
+def process(in_file, out_dir, width, height, phi_samples, theta_samples, xfer_preset):
     # Importing vtk package can be quite slow, only do it if CLI validation passes
     from vtk import (
         vtkMetaImageReader, vtkGPUVolumeRayCastMapper, vtkColorTransferFunction,
@@ -64,15 +136,21 @@ def process(in_file, out_dir, width, height, phi_samples, theta_samples):
     mapper.SetInputConnection(reader.GetOutputPort())
 
     color_function = vtkColorTransferFunction()
-    color_function.AddRGBPoint(field_range[0], 0., 0., 0.)
-    color_function.AddRGBPoint(field_range[1], 1., 1., 1.)
-
     scalar_opacity = vtkPiecewiseFunction()
-    scalar_opacity.AddPoint(field_range[0], 0.)
-    scalar_opacity.AddPoint(field_range[1], 1.)
-
     volume_property = vtkVolumeProperty()
-    #volume_property.ShadeOn()  # TODO(zachmullen) I thought it looked better off, should it be on?
+
+    if xfer_preset is None:  # some sensible naive default
+        color_function.AddRGBPoint(field_range[0], 0., 0., 0.)
+        color_function.AddRGBPoint(field_range[1], 1., 1., 1.)
+        scalar_opacity.AddPoint(field_range[0], 0.)
+        scalar_opacity.AddPoint(field_range[1], 1.)
+    elif xfer_preset in MEDICAL_XFER_PRESETS:
+        build_xfer_fns(
+            color_function, scalar_opacity, volume_property, MEDICAL_XFER_PRESETS[xfer_preset])
+    else:
+        raise Exception('Unknown transfer function preset: %s' % xfer_preset)
+
+
     volume_property.SetInterpolationType(VTK_LINEAR_INTERPOLATION)
     volume_property.SetColor(color_function)
     volume_property.SetScalarOpacity(scalar_opacity)
