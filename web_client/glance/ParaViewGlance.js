c4d2948a778b707e30bb75176ca26709dbbf63c0
Zach Mullen <zach.mullen@kitware.com>
2018-01-25 17:30:42 -0500

    Use modular PV glance dist

--- web_client/glance/ParaViewGlance.js
+++ web_client/glance/ParaViewGlance.js
@@ -1,13 +1,13 @@
 import View from 'girder/views/View';
 
-import 'pv-glance-embeddable';
+import GlanceMain from 'pv-glance-embeddable';
 import template from './paraviewGlance.pug';
 
 const ParaViewGlanceView = View.extend({
     render: function () {
         this.$el.html(template());
 
-        this.glance = new window.pvGlanceMainView(this.$('.g-pv-glance-container')[0], this.model.downloadUrl());
+        this.glance = new GlanceMain(this.$('.g-pv-glance-container')[0], this.model.downloadUrl());
         this.glance.render();
 
         return this;
@@ -15,6 +15,7 @@ const ParaViewGlanceView = View.extend({
 
     destroy: function () {
         this.glance.destroy();
+        View.prototype.destroy.call(this);
     }
 });
 
